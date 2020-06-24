d45de5c4cc8aa64535080e4fa7ea0379c0bb8ede
Yu-Linchien <yu@Yu-LinchiendeMacBook-Pro.local>
2020-06-24 18:11:17 +0800

    test

--- mySql-connect.js
+++ mySql-connect.js
@@ -1,7 +1,6 @@
 const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-<<<<<<< HEAD
   host: "localhost",
   user: "root",
   password: "1234",
@@ -9,20 +8,6 @@ const pool = mysql.createPool({
   // waitForConnections: true,
   // connectionLimit: 10,
   // queueLimit: 0,
-=======
-  // host: "localhost",
-  // user: "root",
-  // password: "",
-  // database: "wow-gym",
-  // waitForConnections: true,
-  // connectionLimit: 10,
-  // queueLimit: 0,
-
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_TABLE,
->>>>>>> e73063d3ca48abaf16a0d0c3c3b5653f512a30b0
 });
 
 // const pool = mysql.createPool({
